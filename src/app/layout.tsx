982e4a0f126dfa69ebdcc3a3e0b147b51b34f8cb
dongho <ovape29@gmail.com>
2026-07-19 17:59:49 +0900

    안녕

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const notoSansKR = Noto_Sans_KR({
 });
 
 export const metadata: Metadata = {
-  title: 'OSS - Ovape Stamp System',
+  title: ' - Ovasssspe Stamp System',
   description: 'Ovape Stamp System - 고객 스탬프 관리 시스템',
   icons: {
     icon: '/icon.png',
